313694aa7fb55b0e5f012f2960518e1f4d11a95d
Harc0506 <harc6@hotmail.com>
2023-07-23 16:36:56 -0600

    Proyecto finalizado

--- script.js
+++ script.js
@@ -1,45 +1,30 @@
-( () => {
-const btn = document.querySelector("[data-form-btn]");
+import checkComplete from './components/checkComplete.js';
+import deleteIcon from './components/deleteIcon.js';
 
-// Arror functions o funciones anonimas
+const btn = document.querySelector('[data-form-btn]');
+
 const createTask = (evento) => {
   evento.preventDefault();
-  const input = document.querySelector("[data-form-input]");
+  const input = document.querySelector('[data-form-input]');
   const value = input.value;
-  const list = document.querySelector("[data-list]");
-  const task = document.createElement("li");
-  task.classList.add("card");
-  input.value = "";
-  console.log(checkComplete());
-  const taskContent = document.createElement("div");
-  taskContent.appendChild(checkComplete());
-  const titleTask = document.createElement("span");
-  titleTask.classList.add("task");
-  titleTask.innerHTML = value;
-  taskContent.appendChild(titleTask);
+  const list = document.querySelector('[data-list]');
+  const task = document.createElement('li');
+  task.classList.add('card');
+  input.value = '';
+  //backticks
+  const taskContent = document.createElement('div');
 
-  const content = `
-  <i class="fas fa-trash-alt trashIcon icon"></i>`;
+  const titleTask = document.createElement('span');
+  titleTask.classList.add('task');
+  titleTask.innerText = value;
+  taskContent.appendChild(checkComplete());
+  taskContent.appendChild(titleTask);
   // task.innerHTML = content;
+
   task.appendChild(taskContent);
+  task.appendChild(deleteIcon());
   list.appendChild(task);
 };
 
-
-btn.addEventListener("click", createTask);
-
-const checkComplete = () => {
-  const i = document.createElement("i");
-  i.classList.add("far","fa-check-square","icon");
-  i.addEventListener("click",completeTask);
-  return i;
-};
-
-// Immediately invoked function expression
-const completeTask = (event) => {
-  const element = event.target;
-  element.classList.toggle("fas");
-  element.classList.toggle("completeIcon")
-  element.classList.toggle("far");
-}
-})();
+//Arrow functions o funciones anonimas
+btn.addEventListener('click', createTask);
